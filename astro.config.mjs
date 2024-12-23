db7562af65afa3965c7e9090d8ca8ddaaf02665e
Juan Sanz <juanunix@users.noreply.github.com>
2024-12-22 23:10:03 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://sanzjv.github.io',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(
-      appBaseStyles: false
-    )
-  ]
-});
+  site:'https://sanzjv.github.io'});
